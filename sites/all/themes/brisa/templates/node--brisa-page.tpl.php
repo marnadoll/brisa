a0f7a9918b1f65d9205cf550921b6a0ccb4e1685
ali <ali@simpleinformation.com>
2015-05-22 15:34:07 -0400

    theme rename and tpl add

---
+++ sites/all/themes/brisa/templates/node--brisa-page.tpl.php
@@ -0,0 +1,19 @@
+<div class="menu" id="menu">
+	<div class="stickyNavInterior">
+<a id="stickyLogo" href="#top"></a>
+<a href="#" id="toggle">Menu</a>
+<ul>
+  <li><a href="#summits">Solutions Summits</a></li>
+  <li><a href="#join">Join The Conversation</a></li>
+  <li><a href="#organize">Organize Your Community</a></li>
+  <li><a href="#incubator">Incubator</a></li>
+  <li><a href="#showcase">Showcase</a></li>
+  <li><a href="#past">VISTA in Images</a></li>
+  <li><a href="#future">$50 For The Future</a></li>
+</ul>
+</div>
+</div>
+
+
+<script src="/sites/all/themes/brisa/js/waypoint.js"></script>
+<script src="/sites/all/themes/brisa/js/scripts.js"></script>
