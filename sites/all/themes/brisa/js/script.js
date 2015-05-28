71ba0346a7b81d7f7ed9b988c3015431cd7bf2dd
ali <ali@simpleinformation.com>
2015-05-28 16:55:18 -0400

    update libs add waypoint

---
+++ sites/all/themes/brisa/js/script.js
@@ -0,0 +1,56 @@
+jQuery(document).ready(function($) {
+  // Target your .container, .wrapper, .post, etc.
+    $(".past_videos").fitVids();
+  
+  // Smooth scrolling
+    $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+  
+  //mobile scripts
+if( jQuery(this).width() < 1100 ) {
+	    
+// Mobile menu
+jQuery('#menu a#toggle').click(function(e) {
+  e.preventDefault();
+  
+  jQuery('#menu ul').slideToggle(200, function() {
+    // Animation complete.
+  });
+});
+
+    };
+
+
+//Desktop scritps
+if( jQuery(this).width() >= 1100 ) {
+	    
+// Hide sticky nav
+jQuery(document.getElementById('menu')).hide();
+
+// Show and hide the menu. uses waypoint.js
+jQuery('.middle').waypoint(function(direction) {
+       if (direction === 'down') {
+     jQuery(document.getElementById('menu')).fadeIn( 500 );
+       }     
+      }, { offset: -120 });
+     
+    
+jQuery('.middle').waypoint(function(direction) {
+       if (direction === 'up') {  
+    jQuery(document.getElementById('menu')).fadeOut( 100 );
+       }     
+      }, { offset: -121 });
+    };
+
+  
+});
